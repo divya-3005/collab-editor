911ded05ac76414495b2816653291bc68685efd5
Divya Singh <divyasorrot3005@gmail.com>
2026-06-22 18:18:13 +0530

    fix: make socket.io connect to production url instead of localhost

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,9 @@
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:3001', {
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api'
+const SOCKET_URL = API_URL.replace('/api', '')
+
+const socket = io(SOCKET_URL, {
   autoConnect: false
 })
 
